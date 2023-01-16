23979342bc6fbaa8e4469dcbcd14adf6fe526b81
Andrew Rodland <andrew@cleverdomain.org>
2023-01-15 19:01:01 -0500

    refactor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"os/signal"
@@ -26,6 +27,7 @@ var cfg struct {
 	Station       string
 	Callsign      string
 	ClusterServer string
+	QRT           bool
 	Timeout       time.Duration
 }
 
@@ -35,6 +37,42 @@ func init() {
 	flag.StringVar(&cfg.Callsign, "callsign", "", "callsign for login")
 	flag.StringVar(&cfg.ClusterServer, "server", "", "cluster server to connect to")
 	flag.DurationVar(&cfg.Timeout, "timeout", 5*time.Minute, "spot persistence timeout")
+	flag.BoolVar(&cfg.QRT, "qrt", true, "delete spots with QRT in comment")
+}
+
+func logToConsole(w io.Writer, m []string, remove bool) {
+	commentColor := color.FgLightCyan
+	if remove {
+		commentColor = color.FgLightRed
+	}
+
+	fmt.Fprintln(
+		w,
+		color.FgLightGreen.Render("SPOT")+" "+
+			"DX de "+color.FgYellow.Render(m[1])+
+			m[2]+color.FgLightBlue.Render(m[3])+
+			m[4]+color.FgMagenta.Render(m[5])+
+			m[6]+commentColor.Render(m[7])+
+			m[8]+m[9],
+	)
+}
+
+func sendToFlex(fc *flexclient.FlexClient, m []string, remove bool) {
+	spotCall, freq, dxCall, comment := m[1], m[3], m[5], m[7]
+	freqKhz, err := strconv.ParseFloat(freq, 64)
+	if err != nil {
+		log.Error().Err(err).Send()
+		return
+	}
+	strings.ReplaceAll(spotCall, " ", "\x7f")
+	strings.ReplaceAll(freq, " ", "\x7f")
+	strings.ReplaceAll(dxCall, " ", "\x7f")
+	strings.ReplaceAll(comment, " ", "\x7f")
+	lifetimeSecs := int(cfg.Timeout / time.Second)
+	res := fc.SendAndWait(fmt.Sprintf("spot add rx_freq=%f callsign=%s spotter_callsign=%s comment=%s lifetime_seconds=%d", freqKhz/1000.0, dxCall, spotCall, comment, lifetimeSecs))
+	if res.Error != 0 {
+		log.Error().Uint32("error", res.Error).Msg(res.Message)
+	}
 }
 
 func main() {
@@ -89,44 +127,13 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		lifetimeSecs := int(cfg.Timeout / time.Second)
-
 		lines := bufio.NewScanner(tc)
 		for lines.Scan() {
 			line := lines.Text()
 			if m := spotPattern.FindStringSubmatch(line); m != nil {
-				spotCall, freq, dxCall, comment, ts := m[1], m[3], m[5], m[7], m[9]
-				freqKhz, err := strconv.ParseFloat(freq, 64)
-				if err != nil {
-					log.Error().Err(err).Send()
-					continue
-				}
-
-				var commentColor = color.FgLightCyan
-				if qrtPattern.MatchString(comment) {
-					// TODO: actually remove the spot. That will need some
-					// refactoring to remember the IDs of spots we add, though.
-					commentColor = color.FgLightRed
-				}
-
-				fmt.Fprintln(
-					rl.Stdout(),
-					color.FgLightGreen.Render("SPOT")+" "+
-						"DX de "+color.FgYellow.Render(spotCall)+
-						m[2]+color.FgLightBlue.Render(freq)+
-						m[4]+color.FgMagenta.Render(dxCall)+
-						m[6]+commentColor.Render(comment)+
-						m[8]+ts,
-				)
-
-				strings.ReplaceAll(spotCall, " ", "\x7f")
-				strings.ReplaceAll(freq, " ", "\x7f")
-				strings.ReplaceAll(dxCall, " ", "\x7f")
-				strings.ReplaceAll(comment, " ", "\x7f")
-				res := fc.SendAndWait(fmt.Sprintf("spot add rx_freq=%f callsign=%s spotter_callsign=%s comment=%s lifetime_seconds=%d", freqKhz/1000.0, dxCall, spotCall, comment, lifetimeSecs))
-				if res.Error != 0 {
-					log.Error().Uint32("error", res.Error).Msg(res.Message)
-				}
+				remove := cfg.QRT && qrtPattern.MatchString(m[7])
+				logToConsole(rl.Stdout(), m, remove)
+				sendToFlex(fc, m, remove)
 			} else {
 				var prompt = false
 				for _, suffix := range promptSuffixes {
