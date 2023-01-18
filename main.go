1d1e3e168793a8d679829a7d5f99f74e38f68dc9
Andrew Rodland <andrew@cleverdomain.org>
2023-01-18 17:13:32 -0500

    Add the ability to "track" and modify/delete spots.
    By default, a callsign is only expected to occur once per band, and if
    they show up at a new frequency within the band, the old spot will be
    moved to the new frequency. If this is disabled with
    `-one-per-band false`, updating will only happen if there is a new
    spot on the same frequency or within 1kHz.
    
    By default, a spot from the cluster with a comment containing "QRT"
    will cause the spot to be deleted from the Flex. This can be disabled
    with `-qrt false`.

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ var cfg struct {
 	Callsign      string
 	ClusterServer string
 	QRT           bool
+	OnePerBand    bool
 	Timeout       time.Duration
 }
 
@@ -38,6 +39,7 @@ func init() {
 	flag.StringVar(&cfg.ClusterServer, "server", "", "cluster server to connect to")
 	flag.DurationVar(&cfg.Timeout, "timeout", 5*time.Minute, "spot persistence timeout")
 	flag.BoolVar(&cfg.QRT, "qrt", true, "delete spots with QRT in comment")
+	flag.BoolVar(&cfg.OnePerBand, "one-per-band", true, "expect a given callsign only once per band")
 }
 
 func logToConsole(w io.Writer, m []string, remove bool) {
@@ -46,16 +48,69 @@ func logToConsole(w io.Writer, m []string, remove bool) {
 		commentColor = color.FgLightRed
 	}
 
+	freqKhz, err := strconv.ParseFloat(m[3], 64)
+	if err != nil {
+		log.Error().Err(err).Send()
+		return
+	}
 	fmt.Fprintln(
 		w,
-		color.FgLightGreen.Render("SPOT")+" "+
-			"DX de "+color.FgYellow.Render(m[1])+
+		color.FgLightGreen.Render("DX de")+
+			" "+color.FgYellow.Render(m[1])+
 			m[2]+color.FgLightBlue.Render(m[3])+
 			m[4]+color.FgMagenta.Render(m[5])+
 			m[6]+commentColor.Render(m[7])+
-			m[8]+m[9],
+			m[8]+m[9]+
+			" "+color.FgLightGreen.Render(getBand(freqKhz)),
 	)
 }
+
+type bandEdge struct {
+	minFreq float64
+	name    string
+}
+
+var bandEdges = []bandEdge{
+	{0, "LFMF"},
+	{1800, "160m"},
+	{3500, "80m"},
+	{5000, "60m"},
+	{7000, "40m"},
+	{10000, "30m"},
+	{14000, "20m"},
+	{18000, "17m"},
+	{21000, "15m"},
+	{24890, "12m"},
+	{26000, "11m"},
+	{28000, "10m"},
+	{50000, "6m"},
+	{144000, "2m"},
+	{220000, "125cm"},
+	{420000, "70cm"},
+	{900000, "900M"},
+	{1240000, "1240M"},
+	{2300000, "microwave"},
+}
+
+func getBand(freq float64) string {
+	i := 0
+	for i < len(bandEdges)-1 && freq >= bandEdges[i+1].minFreq {
+		i++
+	}
+	return bandEdges[i].name
+}
+
+type spotKey struct {
+	freq string
+	call string
+}
+
+type spot struct {
+	id      int
+	expires time.Time
+}
+
+var spotIds = map[spotKey]spot{}
 
 func sendToFlex(fc *flexclient.FlexClient, m []string, remove bool) {
 	spotCall, freq, dxCall, comment := m[1], m[3], m[5], m[7]
@@ -64,14 +119,71 @@ func sendToFlex(fc *flexclient.FlexClient, m []string, remove bool) {
 		log.Error().Err(err).Send()
 		return
 	}
+	var key spotKey
+	if cfg.OnePerBand {
+		key = spotKey{freq: getBand(freqKhz), call: dxCall}
+	} else {
+		key = spotKey{freq: fmt.Sprintf("%.0f", freqKhz), call: dxCall} // round to nearest kHz
+	}
+
 	strings.ReplaceAll(spotCall, " ", "\x7f")
 	strings.ReplaceAll(freq, " ", "\x7f")
 	strings.ReplaceAll(dxCall, " ", "\x7f")
 	strings.ReplaceAll(comment, " ", "\x7f")
+
+	if remove {
+		removeSpot(fc, key)
+	} else {
+		addSpot(fc, key, spotCall, freqKhz, dxCall, comment)
+	}
+}
+
+func addSpot(fc *flexclient.FlexClient, key spotKey, spotCall string, freqKhz float64, dxCall, comment string) {
 	lifetimeSecs := int(cfg.Timeout / time.Second)
-	res := fc.SendAndWait(fmt.Sprintf("spot add rx_freq=%f callsign=%s spotter_callsign=%s comment=%s lifetime_seconds=%d", freqKhz/1000.0, dxCall, spotCall, comment, lifetimeSecs))
+	fields := fmt.Sprintf("rx_freq=%f callsign=%s spotter_callsign=%s comment=%s lifetime_seconds=%d", freqKhz/1000.0, dxCall, spotCall, comment, lifetimeSecs)
+
+	var res flexclient.CmdResult
+	sp, existed := spotIds[key]
+	if existed {
+		// Spot already exists for band/mode, update instead of adding
+		res = fc.SendAndWait(fmt.Sprintf("spot set %d %s", sp.id, fields))
+	} else {
+		res = fc.SendAndWait(fmt.Sprintf("spot add %s", fields))
+	}
+
 	if res.Error != 0 {
 		log.Error().Uint32("error", res.Error).Msg(res.Message)
+		return
+	}
+
+	if !existed {
+		id, err := strconv.Atoi(res.Message)
+		if err != nil {
+			log.Error().Err(err).Msg("atoi")
+			return
+		}
+		sp.id = id
+	}
+	spotIds[key] = spot{id: sp.id, expires: time.Now().Add(cfg.Timeout)}
+}
+
+func removeSpot(fc *flexclient.FlexClient, key spotKey) {
+	spot, ok := spotIds[key]
+	if ok {
+		res := fc.SendAndWait(fmt.Sprintf("spot remove %d", spot.id))
+		if res.Error != 0 {
+			log.Error().Uint32("error", res.Error).Msg(res.Message)
+		}
+	}
+	delete(spotIds, key)
+}
+
+func cleanupSpots() {
+	now := time.Now()
+	for k, v := range spotIds {
+		if v.expires.Before(now) {
+			delete(spotIds, k)
+		}
 	}
 }
 
@@ -134,6 +246,7 @@ func main() {
 				remove := cfg.QRT && qrtPattern.MatchString(m[7])
 				logToConsole(rl.Stdout(), m, remove)
 				sendToFlex(fc, m, remove)
+				cleanupSpots()
 			} else {
 				var prompt = false
 				for _, suffix := range promptSuffixes {
